1e5082f783285561d554b3d0b8d9daa6c6eadc20
Gajindu Gunarathna <gajindugunarathna@gmail.com>
2026-03-20 14:09:44 +0530

    Inti Commit

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF1B5E20);
+  static const Color primaryLight = Color(0xFF4CAF50);
+  static const Color background = Color(0xFFEFF7F0);
+  static const Color cardBackground = Color(0xFFFFFFFF);
+  static const Color textPrimary = Color(0xFF1A1A1A);
+  static const Color textSecondary = Color(0xFF6B6B6B);
+  static const Color error = Color(0xFFD32F2F);
+  static const Color warning = Color(0xFFF57C00);
+  static const Color success = Color(0xFF388E3C);
+  static const Color offlineBanner = Color(0xFFFF8F00);
+}
